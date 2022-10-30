57356ddf7e15be02460229247ac6040bb18d2380
dncuong <dinhcuongnd2001@gmail.com>
2022-10-30 11:53:54 +0700

    B3

--- RJ2209R1/home/src/App.js
+++ RJ2209R1/home/src/App.js
@@ -1,13 +1,13 @@
-import ContractForm from "./components/ContractForm";
+// import ContractForm from "./components/ContractForm";
 import ManageBooks from "./components/ManageBooks";
-
+import ContractForm from "./components/B3";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      <ManageBooks />
+      <ContractForm />
     </div>
   );
 }
